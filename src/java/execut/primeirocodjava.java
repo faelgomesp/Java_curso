49e8617f331a6e3ee1f5ce5908cef1f018fd65ed
faelgomesp <rdomingos174@gmail.com>
2023-05-17 23:41:51 -0300

    Adicionei novos codigos

--- src/java/execut/primeirocodjava.java
+++ src/java/execut/primeirocodjava.java
@@ -5,5 +5,16 @@ public class primeirocodjava {
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro codigo em java");
 		System.out.println("Uma das primeiras aulas em Java");
+		
+		int maiorIdade = 18;
+		int idosoIdade = 60;
+		int jovemIdade = 16;
+		int criancaIdade = 7;
+		String cpf = "154.524.651.22";
+		int cpf2 = 0;
+		System.out.println("Maior idade é =" + maiorIdade);
+		System.out.println("Idoso idade é =" + idosoIdade);
+		System.out.println("Jovem idade é =" + jovemIdade);
 	}
+	
 }
