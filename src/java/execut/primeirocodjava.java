719b44bc330a9e2204fef446b86462bde5829e7f
faelgomesp <rdomingos174@gmail.com>
2023-05-25 22:51:07 -0300

    continuação

--- src/java/execut/primeirocodjava.java
+++ src/java/execut/primeirocodjava.java
@@ -1,20 +1,23 @@
 package java.execut;
 
 public class primeirocodjava {
+	/*Variavel Global e acessivel a todos e o seu valor é compartilhado*/
+	static int maiorIdadeGlobal = 30;
+	
 	/*codigo main auto exexutavel em java*/
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro codigo em java");
 		System.out.println("Uma das primeiras aulas em Java");
 		
+		/*Variavel local porque pertence somente a esse método e o valor fica dentro do método*/
 		int maiorIdade = 18;
-		int idosoIdade = 60;
-		int jovemIdade = 16;
-		int criancaIdade = 7;
-		String cpf = "154.524.651.22";
-		int cpf2 = 0;
-		System.out.println("Maior idade é =" + maiorIdade);
-		System.out.println("Idoso idade é =" + idosoIdade);
-		System.out.println("Jovem idade é =" + jovemIdade);
+		System.out.println("Valor variavel local " + maiorIdade);
+		System.out.println("Valor variavel local Global" + maiorIdadeGlobal);
+			metodo2();
+	}
+	public static void metodo2() {
+		int mediaAno = 50;
+			System.out.println("Valor da variavel Global =" + maiorIdadeGlobal);
 	}
 	
 }
