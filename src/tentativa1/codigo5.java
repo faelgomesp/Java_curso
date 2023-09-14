b3af67cb0f388662f72d13cb880fb7583532f9a7
Rafael Gomes <123125031+faelgomesp@users.noreply.github.com>
2023-09-14 19:03:32 -0300

    Add files via upload

--- src/tentativa1/codigo5.java
+++ src/tentativa1/codigo5.java
@@ -3,13 +3,14 @@ package tentativa1;
 public class codigo5 {
 	public static void main(String[] args) {
 		/*string no Java sempre sera um texto de tamanho qualquer*/
-	String textoQualquer = "khsadvhas ihoadhasdlksaouw ihyasih";	
-	String nome;
-	String cpf;
-	String telefone;
-	String eletronico;
+		/*concatenação de dados*/	
+	String nome= "Rafael";
+	String cpf = "1123.541.321-44";
+	String telefone= "32898428852";
+	String endereco = "BH";
 	
-	
+	System.out.println("Meu nome é :"+ nome + "e o CPF é:"+cpf+ "o telefone é:" + telefone +
+			"o endereco"+ endereco);
 	}
 
 }
---
+++ src/tentativa1/codigo6.java
@@ -0,0 +1,10 @@
+package tentativa1;
+
+public class codigo6 {
+	public static void main(String[] args) {
+		
+		
+	
+	}
+
+}
